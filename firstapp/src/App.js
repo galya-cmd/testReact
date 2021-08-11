276d9dc9dc8571364387020e3794b477385f56b3
galya-cmd <galya1985@bk.ru>
2021-08-12 00:29:41 +0300

    2 вариант

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,16 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactSvgPieChart from "react-svg-piechart"
+ 
+const data = [
+  {title: "Data 1", value: 100, color: "#22594e"},
+  {title: "Data 2", value: 60, color: "#2f7d6d"},
+  {title: "Data 3", value: 30, color: "#3da18d"},
+  {title: "Data 4", value: 20, color: "#69c2b0"},
+  {title: "Data 5", value: 10, color: "#a1d9ce"},
+]
 
 
 function App() {
   return ( 
     <div className = "App" >
-    <div className="container">
-    <h1>Build React Graphs The Easy Way</h1>
-    <div className="section">
-      <Donut />
-    </div>
-  </div>
+      const MyCompo = () = (
+  <ReactSvgPieChart
+    data={data}
+    // If you need expand on hover (or touch) effect
+    expandOnHover
+    // If you need custom behavior when sector is hovered (or touched)
+    onSectorHover={(d, i, e) => {
+      if (d) {
+        console.log("Mouse enter - Index:", i, "Data:", d, "Event:", e)
+      } else {
+        console.log("Mouse leave - Index:", i, "Event:", e)
+      }
+    }}
+  />
+)
     </div>
   );
 }
